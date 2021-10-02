883dc204037c8eada6e1f3fef269f200359ce37e
mac411 <mcman351@gmail.com>
2021-10-01 20:39:03 -0700

    More changes to source and laucnh files

--- src/teleoperator_code.cpp
+++ src/teleoperator_code.cpp
@@ -6,7 +6,6 @@
 
 geometry_msgs::Twist desired_velocity;
 sensor_msgs::LaserScan lidar_reading;
-// lidar_reading.ranges = vector<float>;
 geometry_msgs::Twist command_velocity;
 
 void velCallback(const geometry_msgs::Twist::ConstPtr& vel_msg)
@@ -33,18 +32,21 @@ int main(int argc, char **argv)
 
   ros::Rate loop_rate(10); 
 
-  /**
-   * A count of how many messages we have sent. This is used to create
-   * a unique string for each message.
-   */
-  int count = 0;
   while (ros::ok())
   {
-    if (lidar_reading.ranges.size() != 0) ROS_INFO("Lidar is: [%f]", lidar_reading.ranges.at(133));
+    if (lidar_reading.ranges.size() != 0) 
+    {
+      ROS_INFO("Lidar is: [%f]", lidar_reading.ranges.at(134));
+    }
+    else  
+    {
+      ROS_DEBUG_THROTTLE(30, "There is no robot on the map");
+    }
+    
 
     command_velocity = desired_velocity;
     if (lidar_reading.ranges.size() != 0 &&
-        lidar_reading.ranges.at(133) < 1.0 &&
+        lidar_reading.ranges.at(134) < 1.0 &&
         command_velocity.linear.x > 0.0)
     {
       command_velocity.linear.x = 0.0;
@@ -55,9 +57,7 @@ int main(int argc, char **argv)
     ros::spinOnce();
 
     loop_rate.sleep();
-    ++count;
   }
-
 
   return 0;
 }
