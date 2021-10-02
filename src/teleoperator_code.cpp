caea0f2c45a292355a5d79d0b215e0e3a0d9d51f
mac411 <mcman351@gmail.com>
2021-10-01 19:49:15 -0700

    Updated source code for the node and launch file

--- src/teleoperator_code.cpp
+++ src/teleoperator_code.cpp
@@ -4,72 +4,34 @@
 #include <geometry_msgs/Twist.h>
 #include <sstream>
 
-/**
- * This tutorial demonstrates simple sending of messages over the ROS system.
- */
-void des_velCallback(const geometry_msgs::Twist::ConstPtr& msg)
-{
+geometry_msgs::Twist desired_velocity;
+sensor_msgs::LaserScan lidar_reading;
+// lidar_reading.ranges = vector<float>;
+geometry_msgs::Twist command_velocity;
 
+void velCallback(const geometry_msgs::Twist::ConstPtr& vel_msg)
+{
+  desired_velocity.angular = vel_msg->angular;
+  desired_velocity.linear = vel_msg->linear;
+}
+
+void lidarCallback(const sensor_msgs::LaserScan::ConstPtr& lidar_msg)
+{
+  lidar_reading.ranges = lidar_msg->ranges;
 }
 
 int main(int argc, char **argv)
 {
-  /**
-   * The ros::init() function needs to see argc and argv so that it can perform
-   * any ROS arguments and name remapping that were provided at the command line.
-   * For programmatic remappings you can use a different version of init() which takes
-   * remappings directly, but for most command-line programs, passing argc and argv is
-   * the easiest way to do it.  The third argument to init() is the name of the node.
-   *
-   * You must call one of the versions of ros::init() before using any other
-   * part of the ROS system.
-   */
   ros::init(argc, argv, "teleoperator");
 
-  /**
-   * NodeHandle is the main access point to communications with the ROS system.
-   * The first NodeHandle constructed will fully initialize this node, and the last
-   * NodeHandle destructed will close down the node.
-   */
   ros::NodeHandle n;
 
-  /**
-   * The advertise() function is how you tell ROS that you want to
-   * publish on a given topic name. This invokes a call to the ROS
-   * master node, which keeps a registry of who is publishing and who
-   * is subscribing. After this advertise() call is made, the master
-   * node will notify anyone who is trying to subscribe to this topic name,
-   * and they will in turn negotiate a peer-to-peer connection with this
-   * node.  advertise() returns a Publisher object which allows you to
-   * publish messages on that topic through a call to publish().  Once
-   * all copies of the returned Publisher object are destroyed, the topic
-   * will be automatically unadvertised.
-   *
-   * The second parameter to advertise() is the size of the message queue
-   * used for publishing messages.  If messages are published more quickly
-   * than we can send them, the number here specifies how many messages to
-   * buffer up before throwing some away.
-   */
-  ros::Publisher teleoperator_pub = n.advertise<geometry_msgs::Twist>("cmd_vel", 1000);
+  ros::Publisher vel_pub = n.advertise<geometry_msgs::Twist>("cmd_vel", 1000);
 
-    /**
-   * The subscribe() call is how you tell ROS that you want to receive messages
-   * on a given topic.  This invokes a call to the ROS
-   * master node, which keeps a registry of who is publishing and who
-   * is subscribing.  Messages are passed to a callback function, here
-   * called chatterCallback.  subscribe() returns a Subscriber object that you
-   * must hold on to until you want to unsubscribe.  When all copies of the Subscriber
-   * object go out of scope, this callback will automatically be unsubscribed from
-   * this topic.
-   *
-   * The second parameter to the subscribe() function is the size of the message
-   * queue.  If messages are arriving faster than they are being processed, this
-   * is the number of messages that will be buffered up before beginning to throw
-   * away the oldest ones.
-   */
-  ros::Subscriber teleoperator_sub = n.subscribe("des_vel", 1000, des_velCallback);
+  ros::Subscriber vel_sub = n.subscribe("des_vel", 1000, velCallback);
+  ros::Subscriber lidar_sub = n.subscribe("laser_1", 1000, lidarCallback);
 
-  ros::Rate loop_rate(1);
+  ros::Rate loop_rate(10); 
 
   /**
    * A count of how many messages we have sent. This is used to create
@@ -78,18 +40,17 @@ int main(int argc, char **argv)
   int count = 0;
   while (ros::ok())
   {
-    /**
-     * This is a message object. You stuff it with data, and then publish it.
-     */
-    geometry_msgs::Twist msg;
+    if (lidar_reading.ranges.size() != 0) ROS_INFO("Lidar is: [%f]", lidar_reading.ranges.at(133));
 
-    /**
-     * The publish() function is how you send messages. The parameter
-     * is the message object. The type of this object must agree with the type
-     * given as a template parameter to the advertise<>() call, as was done
-     * in the constructor above.
-     */
-    teleoperator_pub.publish(msg);
+    command_velocity = desired_velocity;
+    if (lidar_reading.ranges.size() != 0 &&
+        lidar_reading.ranges.at(133) < 1.0 &&
+        command_velocity.linear.x > 0.0)
+    {
+      command_velocity.linear.x = 0.0;
+    }
+
+    vel_pub.publish(command_velocity);
 
     ros::spinOnce();
 
