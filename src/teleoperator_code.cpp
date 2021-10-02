68a731e903f262a8f0b3c296a7fead3f0daaa4e0
mac411 <mcman351@gmail.com>
2021-10-01 22:33:55 -0700

    Final version of the majority of the code

--- src/teleoperator_code.cpp
+++ src/teleoperator_code.cpp
@@ -1,19 +1,26 @@
 #include "ros/ros.h"
-#include "std_msgs/String.h"
 #include <sensor_msgs/LaserScan.h>
 #include <geometry_msgs/Twist.h>
-#include <sstream>
 
+/**
+ * Define global variables to keep track of subscriber messages
+ */
 geometry_msgs::Twist desired_velocity;
 sensor_msgs::LaserScan lidar_reading;
 geometry_msgs::Twist command_velocity;
 
+/**
+ * Callback function for vel_sub
+ */
 void velCallback(const geometry_msgs::Twist::ConstPtr& vel_msg)
 {
   desired_velocity.angular = vel_msg->angular;
   desired_velocity.linear = vel_msg->linear;
 }
 
+/**
+ * Callback function for lidar_sub
+ */
 void lidarCallback(const sensor_msgs::LaserScan::ConstPtr& lidar_msg)
 {
   lidar_reading.ranges = lidar_msg->ranges;
@@ -34,22 +41,24 @@ int main(int argc, char **argv)
 
   while (ros::ok())
   {
+    // If ranges vector in lidar_reading is 0, there is no robot generating lidar messages
     if (lidar_reading.ranges.size() != 0) 
     {
       ROS_INFO("Lidar is: [%f]", lidar_reading.ranges.at(134));
     }
     else  
     {
-      ROS_DEBUG_THROTTLE(30, "There is no robot on the map");
+      ROS_INFO_THROTTLE(15, "There is no robot on the map.");
     }
     
-
+    // Set command velocity message to desired velocity and then check if robot will hit will
     command_velocity = desired_velocity;
     if (lidar_reading.ranges.size() != 0 &&
         lidar_reading.ranges.at(134) < 1.0 &&
         command_velocity.linear.x > 0.0)
     {
       command_velocity.linear.x = 0.0;
+      ROS_INFO("Wall detected.");
     }
 
     vel_pub.publish(command_velocity);
